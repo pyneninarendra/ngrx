54cf16ee4768a7acee3ffc6bbfe8130840ac29db
pyneninarendra@gmail.com <pyneninarendra@gmail.com>
2023-11-30 11:08:49 +0530

    refactoring code using async pipe

--- src/app/counter/counter-button/counter-button.component.ts
+++ src/app/counter/counter-button/counter-button.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { decrement, increment, reset } from '../store/counter.action';
+import { counterState } from '../store/counter.store';
 
 @Component({
   selector: 'app-counter-button',
@@ -10,7 +11,7 @@ import { decrement, increment, reset } from '../store/counter.action';
 export class CounterButtonComponent implements OnInit {
 
 
-  constructor(private store: Store<{counter: {counter: number}}>) { }
+  constructor(private store: Store<{counter: counterState}>) { }
 
   ngOnInit(): void {
 
--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,21 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { counterState } from '../store/counter.store';
+import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css']
 })
-export class CounterOutputComponent implements OnInit {
+export class CounterOutputComponent implements OnInit, OnDestroy {
 
   counter: any
-  constructor(private store: Store<{ counter: { counter: any } }>) { }
+  counter$: Observable<counterState>
+  counterSubscription: Subscription
+  constructor(private store: Store<{ counter: counterState }>) { }
 
   ngOnInit() {
-    this.store.select('counter').subscribe(
-      res => {
-        this.counter = res.counter
-      }
-    )
+    this.counter$ = this.store.select('counter')
+    // this.counterSubscription = this.store.select('counter').subscribe(
+    //   res => { this.counter = res }
+    // )
+  }
+  ngOnDestroy() {
+    if (this.counter$) {
+      this.counterSubscription.unsubscribe()
+    }
   }
 }
